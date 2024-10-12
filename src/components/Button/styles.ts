1451cd0402b534eb25b2c8a68a47320bb7b71bd4
Vinicius Pimentel <vinicius.pimentel.furtado@hotmail.com>
2024-10-12 15:08:27 -0300

    Realizado o desafio com sucesso!

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,17 +1,30 @@
 import styled from 'styled-components';
+import React from 'react';
 
+interface ButtonContainerProps {
+    children: React.ReactNode;
+    onClick?: React.MouseEventHandler<HTMLButtonElement>;
+    disabled?: boolean;
+}
 
-export const ButtonContainer = styled.button`
+export const ButtonContainer = styled.button<ButtonContainerProps>`
     width: 100%;
     height: 42px;
     background-color: #81259D;
     color: #FFF;
-
     border: 1px solid #81259D;
     border-radius: 21px;
+    cursor: pointer; // Padrão do cursor
 
     &:hover {
         opacity: 0.6;
-        cursor:pointer;
     }
-`
+
+    &:disabled {
+        background-color: #ccc; /* Cor de fundo quando desabilitado */
+        color: #666; /* Cor do texto quando desabilitado */
+        cursor: not-allowed; /* Cursor de não permitido */
+        opacity: 0.5; /* Opacidade reduzida */
+    }
+`;
+
